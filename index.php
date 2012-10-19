6b642c1c71c6b49a5a01c1152eff044b7966799d
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-10-18 17:47:49 -0700

    Updated theme to use new getColorboxScripts() function for loading ColorBox scripts.

--- index.php
+++ index.php
@@ -10,13 +10,7 @@
     <?php echo $gallery->getColorboxStyles(1); ?>
     
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-    <script type="text/javascript" src="resources/colorbox/jquery.colorbox.js"></script>
-    
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $("a[rel='colorbox']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});
-        });
-    </script>
+    <?php echo $gallery->getColorboxScripts(); ?>
     
     <?php file_exists('googleAnalytics.inc') ? include('googleAnalytics.inc') : false; ?>
 </head>
